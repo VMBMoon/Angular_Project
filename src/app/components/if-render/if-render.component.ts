d93627343c2c48985143811ab624729503fe1b57
HisuiKitsune <foxnightly2010@gmail.com>
2022-10-20 13:07:47 -0300

    angular events added

--- src/app/components/if-render/if-render.component.ts
+++ src/app/components/if-render/if-render.component.ts
@@ -7,12 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IfRenderComponent implements OnInit {
 
-  canShow: boolean = true;
-  name = 'James Bond';
+  canShow: boolean = false;
+  name: string = 'Dr.No';
+  invaded: string = 'red';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  showName(): void {
+    this.canShow = !this.canShow;
+  }
+  fixEvil(): void {
+    this.name = 'James Bond';
+  }
+  invade(): void {
+    this.name = 'Dr.No';
+  }
 }
