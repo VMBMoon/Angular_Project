e7e7137e8a7803edab6ec4136452755e4e7e5480
HisuiKitsune <foxnightly2010@gmail.com>
2022-10-23 17:00:50 -0300

    edit: hash location / link refs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
@@ -20,6 +20,8 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { VendasComponent } from './pages/vendas/vendas.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -46,6 +48,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     FontAwesomeModule
   ],
   providers: [
+  {provide: LocationStrategy, useClass: HashLocationStrategy}
 
   ],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,8 +10,8 @@ import { Component, OnInit } from '@angular/core';
       <img src="../assets/img/angular-logo.png" alt="Logo" width="105" height="28" class="d-inline-block align-text-top">
     </a>
      <ul class="nav">
-          <a class="nav-link active" aria-current="page" href="Home">Home</a>
-          <a class="nav-link" href="Vendas">Vendas</a>
+          <a class="nav-link active" aria-current="page" href="#/Home">Home</a>
+          <a class="nav-link" href="#/Vendas">Vendas</a>
           <a class="nav-link disabled" href="#">Under Maintenance</a>
     </ul>
     </div>
