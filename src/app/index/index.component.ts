5da5535fefd5d6bff967fc03844a855e0edc10d0
HisuiKitsune <foxnightly2010@gmail.com>
2022-10-30 14:20:04 -0300

    fix: index background visual fix

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
   section {
     position: fixed;
     background: rgb(111,149,237);
-    background: linear-gradient(146.5deg, #c65360 10%, #CB152B 56.2%, #0c0c0f 10%, #2B2C4E 56.3%);
+    background: linear-gradient(145deg, #c65360 10%, #CB152B 51.2%, #0c0c0f 10%, #2B2C4E 51.3%);
     min-height: calc(100vh - 56px - 150px);
   }
 
@@ -23,10 +23,11 @@ import { Component, OnInit } from '@angular/core';
   div.container {
     /*background-color: pink;*/
     margin: auto;
-    margin-top: 12em;
+    /*margin-top: 11em;*/
     display: flex;
     flex-wrap: wrap;
     align-items: flex-start;
+    min-height: calc(100vh - 56px - 150px);
 
   }
   div.text {
@@ -34,12 +35,10 @@ import { Component, OnInit } from '@angular/core';
     display: flex;
     flex-direction:column;
     justify-content: center;
-    align-items: center;
-    align-self: flex-start;
-    margin-right: 10em;
     color: white;
-    width: 650px;
+    width: 400px;
     height: 300px;
+
 
   }
 
@@ -50,6 +49,100 @@ import { Component, OnInit } from '@angular/core';
   div p.version{
     margin-top: 30px;
   }
+
+  /* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {
+  img{
+    height: 200px;
+    width: 200px;
+    margin: auto;
+  }
+  div.container {
+    margin: auto;
+    /*margin-top: 6em;*/
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+  }
+  div.text {
+   margin: auto;
+  }
+}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {
+  img{
+    height: 200px;
+    width: 200px;
+    margin: auto;
+  }
+  div.container {
+    margin: auto;
+    /*margin-top: 6em;*/
+
+
+  }
+  div.text {
+   margin: auto;
+  }
+}
+
+/* Medium devices (landscape tablets, 768px and up) */
+@media only screen and (min-width: 768px) {
+  img{
+    height: 230px;
+    width: 230px;
+    margin: auto;
+  }
+  div.container {
+    margin: auto;
+    /*margin-top: 6em;*/
+
+
+  }
+  div.text {
+   margin: auto;
+   width: 500px;
+  }
+}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {
+  img{
+    height: 280px;
+    width: 280px;
+    margin: auto;
+  }
+  div.container {
+    margin: auto;
+    /*margin-top: 13em;*/
+
+  }
+  div.text {
+   margin: auto;
+   margin-left: -2em;
+   margin-right: 10em;
+  }
+}
+
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {
+  div.container {
+    /*margin-top: 13em;*/
+  }
+  img{
+    height: 300px;
+    width: 300px;
+
+  }
+  div.text {
+   margin: auto;
+   margin-left: -0.5em;
+   margin-right: 15em;
+  }
+
+}
 
 </style>
 <section class="container-fluid">
