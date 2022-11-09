4511a3b78c9c42667ad8087632bb03168c1c345f
HisuiKitsune <foxnightly2010@gmail.com>
2022-11-08 21:16:17 -0300

    feat: implemented new service functions, minor fixes

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,23 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from '../shared/product';
+import { Product } from './../shared/product';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductsService } from 'src/app/services/products.service';
-
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  styleUrls: ['./products-list.component.css'],
 })
 export class ProductsListComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'quantity', 'brand', 'expire_date', 'category'];
-
-
-  constructor(private productsService: ProductsService) { }
-
+  displayedColumns: string[] = [
+    'name',
+    'quantity',
+    'brand',
+    'expire_date',
+    'category',
+    'actions',
+  ];
   products: Product[] = [];
 
-  ngOnInit() : void {
+  public data!: Product;
+  private id: any;
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
     this.products = this.productsService.list();
+    this.data = this.productsService.recover(this.id);
   }
 
+  remove(id: any) {
+    this.productsService.remove(id);
+    this.productsService.refresh();
+  }
+  clear() {
+    this.productsService.clear();
+    this.productsService.refresh();
+  }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,24 +1,50 @@
+import { Product } from './../products/shared/product';
 import { Injectable } from '@angular/core';
-import { Product } from '../products/shared/product';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  url = 'http://localhost:4200/#/Products/List';
 
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-  public saveData(data: Product){
-    localStorage.setItem(data.name.toString(), JSON.stringify(data));
+  getAllProducts() {
+    return this.http.get(this.url);
   }
 
-  public list(): Array<Product>{
+  public saveData(data: Product) {
+    localStorage.setItem(data.id!.toString(), JSON.stringify(data));
+  }
+
+  public list(): Array<Product> {
     const products: Product[] = [];
-    console.log(localStorage.length)
-    for(let i = 0; i < localStorage.length; i++){
+    console.log(localStorage.length);
+    for (let i = 0; i < localStorage.length; i++) {
       products.push(JSON.parse(localStorage.getItem(localStorage.key(i)!)!));
     }
     return products;
+  }
+
+  public recover(id: any): Product {
+    return JSON.parse(localStorage.getItem(id)!);
+  }
+  remove(id: any) {
+    let products: Product[] = this.list();
+    localStorage.getItem(localStorage.key(id)!);
+    let index = products.findIndex((e) => e.id === id);
+    if (index !== -1) {
+      localStorage.removeItem(id)!;
+    }
+    console.log(id);
+    return this.http.delete(`${this.url}/${id}`);
+  }
+  refresh(): void {
+    window.location.reload();
+  }
+
+  clear() {
+    localStorage.clear();
   }
 }
